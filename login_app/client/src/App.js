94ceb6b2e17b31b8e96c7db772f9e3219a7b9606
Maxvolt15 <maxvolt123@gmail.com>
2023-03-15 12:10:01 +0530

    react-app

---
+++ login_app/client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">amazing</a>
+      </header>
+      <main>
+          list product
+        </main>
+    </div>
+  );
+}
+
+export default App;
